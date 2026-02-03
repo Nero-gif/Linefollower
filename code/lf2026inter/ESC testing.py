fe28e934af5a3914f624a32cbdea0d000ac3177a
Jakub Lukšík <jakub.luksik@infis.cz>
2026-02-03 18:38:57 +0100

    turbína ok, ale odletí vozítko

--- code/lf2026inter/ESC testing.py
+++ code/lf2026inter/ESC testing.py
@@ -1,34 +1,44 @@
-from machine import Pin, PWM
 import time
 
-# Doporučuji GPIO 2 (na desce označen jako G2 nebo jen 2)
-# Je to bezpečný pin, který nekoliduje s OLED displejem (I2C)
-servo_pin = Pin(2, Pin.OUT)
-pwm = PWM(servo_pin)
+from machine import Pin, PWM
 
-# Nastavení frekvence na 50Hz (standard pro serva)
-pwm.freq(50)
+esc = PWM(Pin(2))
+esc.freq(50)
 
-print("Startuji PWM na GPIO 2...")
 
-# Inicializace na střed (cca 1.5ms puls)
-# Pro 16-bit (0-65535): 1ms = 3277, 1.5ms = 4915, 2ms = 6553
-pwm.duty_u16(4915)
-time.sleep(2)
+def set_speed(us):
+    duty = int(us * 65535 / 20000)  # 20 ms perioda
+    esc.duty_u16(duty)
+    print(us)
+
 
 try:
-    while True:
-        # Pomalu zvyšuj (pohyb tam)
-        for duty in range(3277, 6553, 50):
-            pwm.duty_u16(duty)
-            time.sleep(0.02) # Trochu rychlejší odezva
+    print("Inicializace ESC...")
+    set_speed(1000)  # minimum
+    time.sleep(5)
 
-        # Pomalu snižuj (pohyb zpět)
-        for duty in range(6553, 3277, -50):
-            pwm.duty_u16(duty)
-            time.sleep(0.02)
+    for i in range(1000, 1250, 10):
+        set_speed(i)
+        time.sleep(2)
+
+    time.sleep(5)
+    # print("Start motoru...")
+    # set_speed(1100)
+    # time.sleep(5)
+    #
+    # print("Speeding...")
+    # set_speed(1200)
+    # time.sleep(5)
+
+    print("Stop motoru...")
+    esc.deinit()
 
 except KeyboardInterrupt:
     # Bezpečné ukončení a vypnutí PWM
-    pwm.deinit()
+    esc.deinit()
     print("PWM vypnuto")
+#
+
+#
+# print("Stop")
+# set_speed(1000)
